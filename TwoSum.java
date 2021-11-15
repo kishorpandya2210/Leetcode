5e0834ac04df1da0495271132e2769713736ebc5
kishorpandya2210 <kishorpandya17@gmail.com>
2021-11-14 22:06:09 -0500

    Added my solution to the two sum question.

---
+++ TwoSum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length; i++){
+            int a = nums[i];
+            for(int j=i+1; j<nums.length;j++){
+                int b = nums[j];
+                if(a+b==target){
+                    int[] ans = {i,j};
+                    return ans;
+                }
+            }
+        }
+        return null;
+    }
+}
